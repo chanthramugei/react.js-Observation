18bef7e110e761558ddb6c205dbe050513b89929
chanthramugei <chanthramugeiboopathi@gmail.com>
2026-01-29 10:09:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React ,{ useState } from 'react'
+
+function Calculator() {
+  const[num1,setNum1]=useState("");
+  const[num2,setNum2]=useState("");
+
+  const a=Number(num1);
+  const b=Number(num2);
+  return (
+    <div>
+      <h1 style={{textDecoration:"underline"}}>Calculator</h1>
+      <input type="number" placeholder='Enter the first value'
+      onChange={(e)=>setNum1(e.target.value)}/>
+      <input type="number" placeholder='Enter the second value'
+      onChange={(e)=>setNum2(e.target.value)}/>
+      <div style={{height:"200px",
+        width:"300px",
+        border:"3px inset black",
+        marginLeft:"260px",
+        marginTop:"20px",
+        backgroundColor:"lightblue",
+        textAlign:"left"}}>
+          <h3>Result</h3>
+          <p><strong>Addition :</strong>{a+b}</p>
+          <p><strong>Subtraction :</strong>{a-b}</p>
+          <p><strong>Multiplication :</strong>{a*b}</p>
+          <p><strong>Division :</strong>
+          {b==0 ?"can't divide by zero":a/b}
+          </p>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Calculator
